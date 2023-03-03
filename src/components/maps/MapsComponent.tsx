cbf5eba11590aca61b9cc3abfde8855ace0b6d25
Ricardo Di Benedetto <richard.u.db@gmail.com>
2023-03-03 15:20:13 -0300

    mapa

--- src/components/maps/MapsComponent.tsx
+++ src/components/maps/MapsComponent.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export const MapsComponent = () => {
   return (
     <div >
+      <h1>¿Donde? <br />
+      Antares.</h1>
       <iframe 
-        className='mapsCard'
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.212796951837!2d-58.40169786965411!3d-34.699812317855084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccce04d1173fd%3A0x9ba15fc15f43643b!2sDAZ%2C%202%20de%20Mayo%202448%2C%20B1824%20Lan%C3%BAs%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1677861351790!5m2!1ses-419!2sar"
+        style={{border: 'none', height: '300px', borderRadius: '16px'}}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.1049506110426!2d-58.37002388443592!3d-34.75294898042107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32d35899bc449%3A0x8f298c49968b1a88!2sDinamarca%202326%2C%20Temperley%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1677866671336!5m2!1ses-419!2sar"
         loading="lazy" 
         ></iframe>
     </div>
