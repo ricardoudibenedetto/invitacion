dc574b2921bd1b8f2ee27248854767c0eb77d061
Ricardo Di Benedetto <richard.u.db@gmail.com>
2023-03-03 17:31:29 -0300

    login center

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import { LoginFormComponent } from '../components/login/LoginFormComponent'
 export const LoginPage = () => {
   return (
     <main className='container margin-top'>
-      <h1>Mis 15 Maia</h1>
+      <h1 className='text-center'>Mis 15 Maia</h1>
       <section>
         <LoginFormComponent />
       </section>
